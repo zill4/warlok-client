4c61f4f16be6d4a92ec6bbac48223a297b83b182
Justin Crisp <justin@crispcode.io>
2025-09-27 18:22:52 -0500

    adding webspatial support

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import webSpatial from "@webspatial/vite-plugin";
+import { createHtmlPlugin } from "vite-plugin-html";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      jsxImportSource: "@webspatial/react-sdk",
+    }),
+    webSpatial(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          XR_ENV: process.env.XR_ENV,
+        },
+      },
+    }),
+  ],
+});
